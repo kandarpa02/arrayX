6587a39d52dcae88493789bcf076d3e03ab6ed84
kandarpa02 <kandarpaexe@gmail.com>
2025-07-16 23:18:29 +0530

    init

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages, Extension
+import os
+
+
+setup(
+    name="NeoNet",
+    version="0.0.1a1",
+    author="Kandarpa Sarkar",
+    author_email="kandarpaexe@gmail.com",
+    description="An autodiff library for personal use",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/kandarpa02/NeoNet.git",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Software Development :: Libraries",
+    ],
+    license="Apache-2.0",
+    zip_safe=False,
+)
