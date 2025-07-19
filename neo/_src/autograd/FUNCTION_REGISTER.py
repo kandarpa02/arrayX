e5b1580c1bdedfdd1ae8a1ef66f96031fbdfd144
kandarpa02 <kandarpaexe@gmail.com>
2025-07-19 22:17:31 +0530

    add logo, readme

--- neo/_src/autograd/FUNCTION_REGISTER.py
+++ neo/_src/autograd/FUNCTION_REGISTER.py
@@ -1,10 +1,19 @@
 from typing import Any
 from dataclasses import dataclass
+from neo.backend import get_xp
+
+def define_device(x):
+    import numpy as np
+    device = 'cpu'
+    if not isinstance(x, np.ndarray):
+        device = 'cuda'
+    return device
 
 @dataclass
 class context:
     def save(self, *args):
         self.data = args
+
     @property
     def release(self):
         return self.data
@@ -12,6 +21,7 @@ class context:
 class Policy:
     def __init__(self):
         self.ctx = context()
+        self.nx = get_xp(define_device(self.ctx.data[0]))
     
     def forward(self, *args):
         raise NotImplementedError
