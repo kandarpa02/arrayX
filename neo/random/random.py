33cc2a6569d1a64a46244d8aa44abee9e5ca8c13
kandarpa02 <kandarpaexe@gmail.com>
2025-07-20 08:56:41 +0530

    add dtype arg in random functions

--- neo/random/random.py
+++ neo/random/random.py
@@ -1,11 +1,11 @@
 from neo.backend import get_xp
 from neo.numpy import array
 
-def randn(shape, device=None, dtype = 'float32'):
+def randn(shape, device='cpu', dtype = 'float32'):
     xp = get_xp(device)
-    return array(xp.asarray(xp.random.randn(*shape)), dtype=dtype device=device)
+    return array(xp.asarray(xp.random.randn(*shape)), dtype=dtype, device=device)
 
-def randint(low, high=None, size=None, device=None, dtype = 'float32'):
+def randint(low, high=None, size=None, device='cpu', dtype = 'float32'):
     xp = get_xp(device)
     return array(xp.asarray(xp.random.randint(low, high, size)), dtype=dtype, device=device)
 
