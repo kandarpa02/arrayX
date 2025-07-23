9c9ef02c22d2976d0e0a5dafa8b9a743b36ef2eb
kandarpa02 <kandarpaexe@gmail.com>
2025-07-23 21:10:46 +0530

    fix

--- neo/neomizer/_opt_wrapper.py
+++ neo/neomizer/_opt_wrapper.py
@@ -24,11 +24,12 @@ class NeoOptimizer:
         )
 
     def step(self, grads: dict[Any, torch.Tensor]) -> dict[Any, Any]:
-        grad = grads.get(p, None)
-        if grad is not None:
-            if grad.shape != p.data.shape:
-                grad = grad.expand_as(p.data) 
-            p.data.grad = grad
+        for p in self.params.values():
+            grad = grads.get(p, None)
+            if grad is not None:
+                if grad.shape != p.data.shape:
+                    grad = grad.expand_as(p.data)
+                p.data.grad = grad
 
 
         self.optimizer.step()
