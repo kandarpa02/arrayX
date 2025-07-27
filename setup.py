7afe5839d893abd8da3d6267d748f32b8b71850d
kandarpa02 <kandarpaexe@gmail.com>
2025-07-27 14:42:38 +0530

    setup

--- setup.py
+++ setup.py
@@ -1,45 +1,36 @@
 from setuptools import setup, find_packages, Extension
 from Cython.Build import cythonize
-import os
 
+# Load dependencies from requirements.txt
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+# Load long description from README.md
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+# Define Cython extension
 ext_modules = cythonize([
     Extension(
         name="neo._src.autograd.GRAPH_MANAGER",
         sources=["neo/_src/autograd/GRAPH_MANAGER.pyx"],
         language="c++",
     ),
-    
 ])
 
-
-def if_not_available(requirements:list):
-    req_to_install = []
-    for req in requirements:
-        try:
-            import req
-        except:
-            print(f"{req} is being installed")
-            req_to_install.append(req)
-    return req_to_install
-
-req_list = []
-with open("requirements.txt", "r") as f:
-    req_list.append(f.read())
-
-req_to_install = if_not_available(req_list)
-
+# Setup configuration
 setup(
     name="neonet",
     version="0.0.1a1",
     author="Kandarpa Sarkar",
     author_email="kandarpaexe@gmail.com",
     description="An autodiff library for personal use",
-    long_description=open("README.md", encoding="utf-8").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/kandarpa02/neonet.git",
     packages=find_packages(),
     ext_modules=ext_modules,
-    install_requires=req_to_install,
+    install_requires=requirements,
     python_requires=">=3.8",
     classifiers=[
         "Programming Language :: Python :: 3",
