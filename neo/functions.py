9af2980fd35de2758b1cad39e10ac0ec8fb5ae80
kandarpa02 <kandarpaexe@gmail.com>
2025-08-04 23:08:09 +0530

    changed api

--- neo/functions.py
+++ neo/functions.py
@@ -16,43 +16,6 @@ def neo_function(fn):
     return function(fn)  
 
 
-# def function(fn_object: Callable):
-#     from neo._torch.lite_tensor import LiteTensor
-#     from neo._src.autograd import GRAPH_MANAGER
-
-#     def wrapped(*args, **kwargs):
-#         op = fn_object()
-
-#         valargs = []
-#         valargs_strict = []
-#         auxargs = []
-
-#         for arg in args:
-#             if isinstance(arg, LiteTensor):
-#                 valargs.append(arg.data)
-#                 valargs_strict.append(arg)
-#             elif isinstance(arg, (neolib.Tensor, int, float)):
-#                 valargs.append(arg)
-#             elif isinstance(arg, (bool, type(None), tuple, list)):
-#                 auxargs.append(arg)
-#             else:
-#                 raise TypeError(f"Unsupported argument type: {type(arg)}")
-
-#         for key, val in kwargs.items():
-#             if isinstance(val, (int, bool)):
-#                 auxargs.append(val)
-#             else:
-#                 raise TypeError(f"Unsupported keyword argument {key}={val} in function")
-
-#         out_val = op.forward(*valargs, *auxargs)
-#         out = LiteTensor(out_val)
-        
-#         node = GRAPH_MANAGER.Node(out, tuple(valargs_strict), op.backward)
-#         GRAPH_MANAGER.TapeContext.add_node(node)
-#         return out
-        
-#     return wrapped
-
 def function(fn_object: Callable):
     from neo._torch.lite_tensor import LiteTensor
     from neo._src.autograd import GRAPH_MANAGER
