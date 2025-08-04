f0f6fabe9c12b53023400c3755e7caca9a7dddeb
kandarpa02 <kandarpaexe@gmail.com>
2025-08-04 19:02:09 +0530

    changed api

--- neo/_torch/user_functions.py
+++ neo/_torch/user_functions.py
@@ -2,28 +2,29 @@ from neo._torch.lite_tensor import LiteTensor
 from neo._torch import neolib
 from ._helper import _dtype, _device
 
-def Lite(data, dtype='', device=''):
+def data(data, dtype='', device=''):
+    data = data.data if isinstance(data, LiteTensor) else data
     return LiteTensor(data=data, d_type=dtype, device=device)
 
 
 # === Tensor Constructors ===
 
 def full(shape, fill_value, dtype='', device=''):
-    return Lite(neolib.full(
+    return data(neolib.full(
         shape, fill_value,
         dtype=_dtype(dtype) if dtype else None,
         device=_device(device) if device else None
     ))
 
 def ones(shape, dtype='', device=''):
-    return Lite(neolib.ones(
+    return data(neolib.ones(
         shape,
         dtype=_dtype(dtype) if dtype else None,
         device=_device(device) if device else None
     ))
 
 def zeros(shape, dtype='', device=''):
-    return Lite(neolib.zeros(
+    return data(neolib.zeros(
         shape,
         dtype=_dtype(dtype) if dtype else None,
         device=_device(device) if device else None
@@ -34,27 +35,27 @@ def arange(start, end=None, step=1, dtype='', device=''):
     device = _device(device) if device else None
 
     if end is None:
-        return Lite(neolib.arange(0, start, step, dtype=dtype, device=device))
+        return data(neolib.arange(0, start, step, dtype=dtype, device=device))
     else:
-        return Lite(neolib.arange(start, end, step, dtype=dtype, device=device))
+        return data(neolib.arange(start, end, step, dtype=dtype, device=device))
 
 
 def empty(shape, dtype='', device=''):
-    return Lite(neolib.empty(
+    return data(neolib.empty(
         shape,
         dtype=_dtype(dtype) if dtype else None,
         device=_device(device) if device else None
     ))
 
 def rand(shape, dtype='', device=''):
-    return Lite(neolib.rand(
+    return data(neolib.rand(
         shape,
         dtype=_dtype(dtype) if dtype else None,
         device=_device(device) if device else None
     ))
 
 def randn(shape, dtype='', device=''):
-    return Lite(neolib.randn(
+    return data(neolib.randn(
         shape,
         dtype=_dtype(dtype) if dtype else None,
         device=_device(device) if device else None
@@ -64,21 +65,21 @@ def randn(shape, dtype='', device=''):
 # === _like Constructors ===
 
 def ones_like(x, dtype='', device=''):
-    return Lite(neolib.ones_like(
+    return data(neolib.ones_like(
         x.data,
         dtype=_dtype(dtype) if dtype else None,
         device=_device(device) if device else None
     ))
 
 def zeros_like(x, dtype='', device=''):
-    return Lite(neolib.zeros_like(
+    return data(neolib.zeros_like(
         x.data,
         dtype=_dtype(dtype) if dtype else None,
         device=_device(device) if device else None
     ))
 
 def empty_like(x, dtype='', device=''):
-    return Lite(neolib.empty_like(
+    return data(neolib.empty_like(
         x.data,
         dtype=_dtype(dtype) if dtype else None,
         device=_device(device) if device else None
@@ -88,40 +89,40 @@ def empty_like(x, dtype='', device=''):
 # === Shape/View/Manipulation Ops ===
 
 def reshape(x, *shape):
-    return Lite(x.data.reshape(*shape))
+    return data(x.data.reshape(*shape))
 
 def permute(x, *dims):
-    return Lite(x.data.permute(*dims))
+    return data(x.data.permute(*dims))
 
 def transpose(x, dim0, dim1):
-    return Lite(x.data.transpose(dim0, dim1))
+    return data(x.data.transpose(dim0, dim1))
 
 def squeeze(x, dim=None):
-    return Lite(x.data.squeeze(dim)) if dim is not None else Lite(x.data.squeeze())
+    return data(x.data.squeeze(dim)) if dim is not None else data(x.data.squeeze())
 
 def unsqueeze(x, dim):
-    return Lite(x.data.unsqueeze(dim))
+    return data(x.data.unsqueeze(dim))
 
 def flatten(x, start_dim=0, end_dim=-1):
-    return Lite(x.data.flatten(start_dim, end_dim))
+    return data(x.data.flatten(start_dim, end_dim))
 
 def view(x, shape):
-    return Lite(x.data.view(*shape))
+    return data(x.data.view(*shape))
 
 def expand(x, shape):
-    return Lite(x.data.expand(*shape))
+    return data(x.data.expand(*shape))
 
 def repeat(x, repeats):
-    return Lite(x.data.repeat(*repeats))
+    return data(x.data.repeat(*repeats))
 
 def cat(tensors, dim=0):
-    return Lite(neolib.cat([t.data for t in tensors], dim=dim))
+    return data(neolib.cat([t.data for t in tensors], dim=dim))
 
 def stack(tensors, dim=0):
-    return Lite(neolib.stack([t.data for t in tensors], dim=dim))
+    return data(neolib.stack([t.data for t in tensors], dim=dim))
 
 def argmax(x, dim=None, keepdim=False):
-    return Lite(neolib.argmax(x.data, dim=dim, keepdim=keepdim))
+    return data(neolib.argmax(x.data, dim=dim, keepdim=keepdim))
 
 def argmin(x, dim=None, keepdim=False):
-    return Lite(neolib.argmin(x.data, dim=dim, keepdim=keepdim))
+    return data(neolib.argmin(x.data, dim=dim, keepdim=keepdim))
