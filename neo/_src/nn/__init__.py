f0f6da47e9738620b4a325196f6c40175889bac6
kandarpa02 <kandarpaexe@gmail.com>
2025-08-27 11:49:03 +0530

    conv layers added

--- neo/_src/nn/__init__.py
+++ neo/_src/nn/__init__.py
@@ -5,4 +5,5 @@ from ._activations import *
 from ._losses import softmax_cross_entropy
 from ..nn import layers
 from .dense_layer import Dense
+from .conv_layer import *
 from ..nn import initializers as Initializers
