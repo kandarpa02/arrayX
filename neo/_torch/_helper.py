e18af6c715fef82fa6705348f2bd40d547bc01f4
Kandarpa Sarkar <kandarpaexe@gmail.com>
2025-08-29 16:58:43 +0530

    Update _helper.py

--- neo/_torch/_helper.py
+++ neo/_torch/_helper.py
@@ -1,22 +1,23 @@
 import torch
 
 def _device(arg):
-    if arg is None:
-        return None
-    if isinstance(arg, torch.device):
-        return arg
-    if isinstance(arg, str):
-        return torch.device(arg)
+    """Ensure argument is a torch.device"""
+    if arg is None: return None
+    if isinstance(arg, torch.device): return arg
+    if isinstance(arg, str): return torch.device(arg)
     raise TypeError(f"Invalid device: {arg}")
 
 def _dtype(arg):
-    if arg is None:
-        return None
-    if isinstance(arg, torch.dtype):
-        return arg
+    """Ensure argument is torch.dtype"""
+    if arg is None: return None
+    if isinstance(arg, torch.dtype): return arg
     if isinstance(arg, str):
-        try:
-            return getattr(torch, arg)
-        except AttributeError:
-            raise TypeError(f"Invalid dtype string: '{arg}'")
+        try: return getattr(torch, arg)
+        except AttributeError: raise TypeError(f"Invalid dtype string: '{arg}'")
     raise TypeError(f"Invalid dtype: {arg}")
+
+def _neo_dtype(arg):
+    """Convert torch dtype to string representation without torch. prefix"""
+    arg = str(arg)
+    if 'torch.' in arg:
+        return arg.removeprefix('torch.')
