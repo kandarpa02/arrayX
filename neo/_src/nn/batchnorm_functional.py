66b05e771d8f94035e1dbb1640431f5a05e89607
kandarpa02 <kandarpaexe@gmail.com>
2025-09-01 18:16:21 +0530

    fix bn backward

--- neo/_src/nn/batchnorm_functional.py
+++ neo/_src/nn/batchnorm_functional.py
@@ -3,7 +3,7 @@ import neo
 from neo._src.autograd.FUNCTION_REGISTER import Tracelet
 from neo._torch.lite_tensor import LiteTensor
 
-def batchnorm2d(
+def batchnorm2d_fused(
     x: LiteTensor,
     gamma: LiteTensor,
     beta: LiteTensor,
@@ -16,28 +16,30 @@ def batchnorm2d(
 
     C = x.data.shape[1]
 
+    # init running stats if None
     if running_mean is None:
         running_mean = neo.zeros((C,), dtype=x.dtype)
     if running_var is None:
         running_var = neo.ones((C,), dtype=x.dtype)
 
+    # forward stats
     if train:
         mean = x.data.mean(dim=(0,2,3), keepdim=True)
         var  = x.data.var(dim=(0,2,3), unbiased=False, keepdim=True)
-        updated_running_mean = LiteTensor((1 - momentum) * running_mean.data + momentum * mean.squeeze(), d_type=x.dtype)
-        updated_running_var  = LiteTensor((1 - momentum) * running_var.data + momentum * var.squeeze(), d_type=x.dtype)
+        updated_running_mean = LiteTensor((1-momentum)*running_mean.data + momentum*mean.squeeze(), d_type=x.dtype)
+        updated_running_var  = LiteTensor((1-momentum)*running_var.data  + momentum*var.squeeze(), d_type=x.dtype)
     else:
-        mean = running_mean.data.view(1, -1, 1, 1)
-        var  = running_var.data.view(1, -1, 1, 1)
+        mean = running_mean.data.view(1,-1,1,1)
+        var  = running_var.data.view(1,-1,1,1)
         updated_running_mean = running_mean
         updated_running_var  = running_var
 
-    # forward: just use minimal tensors
+    # compute output (forward)
     inv_std = 1.0 / (var + eps).sqrt()
-    out_data = gamma.data.view(1, -1, 1, 1) * (x.data - mean) * inv_std + beta.data.view(1, -1, 1, 1)
+    out_data = gamma.data.view(1,-1,1,1)*(x.data - mean)*inv_std + beta.data.view(1,-1,1,1)
     out = LiteTensor(out_data, d_type=x.dtype)
 
-    # backward: recompute everything on the fly
+    # backward: everything recomputed in the closure
     def bn2d_backward(grad, x=x, gamma=gamma):
         xd = x.data
         gd = gamma.data
@@ -45,24 +47,26 @@ def batchnorm2d(
 
         N = xd.shape[0] * xd.shape[2] * xd.shape[3]
 
-        # recompute mean/var per channel
+        # recompute mean/var/inv_std/x_norm per channel
         mean = xd.mean(dim=(0,2,3), keepdim=True)
         var  = xd.var(dim=(0,2,3), unbiased=False, keepdim=True)
         inv_std = 1.0 / (var + eps).sqrt()
         x_norm = (xd - mean) * inv_std
 
-        # dbeta / dgamma
+        # compute param gradients
         dbeta  = grad.sum(dim=(0,2,3))
         dgamma = (grad * x_norm).sum(dim=(0,2,3))
 
-        # compact input gradient
+        # compute input gradient in-place style (minimal temporaries)
         sum_grad = grad.sum(dim=(0,2,3), keepdim=True)
         sum_grad_xnorm = (grad * x_norm).sum(dim=(0,2,3), keepdim=True)
         g_view = gd.view(1, -1, 1, 1)
-        dx = (1.0 / N) * g_view * inv_std * (N * grad - sum_grad - x_norm * sum_grad_xnorm)
+        dx = (1.0 / N) * g_view * inv_std * (N*grad - sum_grad - x_norm*sum_grad_xnorm)
 
+        # return minimal outputs
         return dx, dgamma, dbeta
 
+    # register only minimal things: x and gamma
     with Tracelet() as t:
         t.register(out, (x, gamma), bn2d_backward)
 
